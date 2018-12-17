b075e8f3706c97fa265141414b0f95c5f05e7997
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-12-17 17:30:37 +1000

    Updates

--- page-templates/blocks/text-text.php
+++ page-templates/blocks/text-text.php
@@ -13,11 +13,11 @@ $spaceBelow = get_sub_field('space_below');
 ?>
 
   <div class="container space-below--<?php echo $spaceBelow ?>">
-    <div class="row  <?php if( $flipLayout == 'yes' ): echo ''; endif; ?>">
-        <div class=" flippable__text  <?php if( $flipLayout == 'yes' ): if( $layout == '1/3' ): echo 'col-md-9'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-7'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-5'; endif; else: if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; endif; ?>" >
+    <div class="row justify-content-between <?php if( $flipLayout == 'yes' ): echo ''; endif; ?>">
+        <div class=" flippable__text  <?php if( $flipLayout == 'yes' ): if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-5'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; else: if( $layout == '1/3' ): echo 'col-md-8'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-5'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-4'; endif; endif; ?>" >
             <?php echo $text ?>
         </div>
-        <div class="<?php if( $layout == '1/3' ): echo 'col-md-4'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-8'; endif; ?> flippable__text d-flex align-items-center">
+        <div class="<?php if( $layout == '1/3' ): echo 'col-md-4'; endif; ?> <?php if( $layout == '1/2' ): echo 'col-md-6'; endif; ?> <?php if( $layout == '2/3' ): echo 'col-md-8'; endif; ?> flippable__text">
             <?php echo $text2 ?>
         </div>
     </div>
