a9100b7e949ae81c7e29ddd87291a384860347e5
Elliot <elliot@newwordorder.com.au>
2019-05-28 15:07:34 +1000

    clean up

--- searchform.php
+++ searchform.php
@@ -9,7 +9,8 @@
 
 
 <form class="d-flex" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-			<input class="field form-control mr-2" id="s" name="s" type="text"
+<label class="d-none" for="s">Search</label>			
+<input class="field form-control mr-2" id="s" name="s" type="text"
 					placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
 			<input class="btn btn--solid" id="searchsubmit" name="submit" type="submit"
 					value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
