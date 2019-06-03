7854cd9dd31377a9784b9954fe0e2817e2a5c1cd
Elliot <elliot@newwordorder.com.au>
2019-06-03 15:05:51 +1000

    updates search, bench tag, remove logging

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 <form class="d-flex" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 <label class="d-none" for="s">Search</label>			
 <input class="field form-control mr-2" id="s" name="s" type="text"
-					placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
+					placeholder="<?php esc_attr_e( 'Search The Banyans&hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
 			<input class="btn btn--solid" id="searchsubmit" name="submit" type="submit"
 					value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
 	</form>
